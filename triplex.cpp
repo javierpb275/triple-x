abbe9dd6b783536dacc8d6e36f5149fb7aa90303
javierpb275 <javierpb275@gmail.com>
2022-03-08 19:39:12 +0100

    printing messages

--- triplex.cpp
+++ triplex.cpp
@@ -2,11 +2,8 @@
 
 int main()
 {
-    std::cout << "Hello World!";
+    std::cout << "You are a secret agent breaking into a secure server room...";
+    std::cout << std::endl; //new line
+    std::cout << "Enter the correct code to continue";
 	return 0;
 }
-
-//std: namespace (prevent name conflicts in our code). we access the namespace by using the "::".
-//:: : Scope Operator. we tell the compiler to check the code inside the namespace.
-//cout: to print sth in the console
-// << : allow us to output the text to the right
