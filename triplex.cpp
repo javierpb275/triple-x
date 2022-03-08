bfa0c453f39a05cf175d30c5a58f47ae66567a3d
javierpb275 <javierpb275@gmail.com>
2022-03-08 19:29:36 +0100

    hello world

--- triplex.cpp
+++ triplex.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "Hello World!";
 	return 0;
 }
+
+//std: namespace (prevent name conflicts in our code). we access the namespace by using the "::".
+//:: : Scope Operator. we tell the compiler to check the code inside the namespace.
+//cout: to print sth in the console
+// << : allow us to output the text to the right
