f04743051efd945c4d9bad2871ae47069f07e0c5
javierpb275 <javierpb275@gmail.com>
2022-03-13 12:47:46 +0100

    escape sequence \

--- triplex.cpp
+++ triplex.cpp
@@ -9,9 +9,8 @@ int main()
     //>>: extraction operator
 
     //Print welcome messages to the terminal:
-    std::cout << "You are a secret agent breaking into a secure server room...";
-    std::cout << std::endl;
-    std::cout << "Enter the correct code to continue..." << std::endl;
+    std::cout << "You are a secret agent breaking into a secure server room...\n";
+    std::cout << "Enter the correct code to continue...\n\n";
 
     //Declare 3 number code
     const int CodeA = 4;
@@ -23,27 +22,27 @@ int main()
 
     //Print sum and product to the terminal
     std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "+ There are \"3 numbers\" in the code";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
 
+    //Store player guess
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
-    std::cout << "You entered: " << GuessA << GuessB << GuessC;
-    std::cout << std::endl;
+    std::cin >> GuessA >> GuessB >> GuessC;
+
+    std::cout << "\nYou entered: " << GuessA << GuessB << GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    //check if the player guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "You win!";
+        std::cout << "\nYou win!";
     }
     else
     {
-        std::cout << "You lose!";
+        std::cout << "\nYou lose!";
     }
 
     return 0;
